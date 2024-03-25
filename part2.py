c8701db93ad048ffc163ee7feb61db2dfb44080c
DownByBracket <164828811+DownByBracket@users.noreply.github.com>
2024-03-24 22:34:30 -0500

    Add files via upload

---
+++ part2.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+product = num1 * num2
+quotient = num1 / num2
+
+print("The product of", num1, "and", num2, "is:", product)
+print("The quotient of", num1, "and", num2, "is:", quotient)
